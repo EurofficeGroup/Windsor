ab8a3511363e8f36a4ed65766065a47814233b75
Lukas Tines <ltines@euroffice.co.uk>
2024-05-19 01:44:06 +0100

    Use TryCurrent instead of accessing private fields

--- src/Castle.Windsor.Extensions.DependencyInjection/Scope/ExtensionContainerScope.cs
+++ src/Castle.Windsor.Extensions.DependencyInjection/Scope/ExtensionContainerScope.cs
@@ -35,9 +35,9 @@ namespace Castle.Windsor.Extensions.DependencyInjection.Scope
 
 		public override void Dispose()
 		{
-			if (ExtensionContainerScopeCache.current.Value == this)
+			if (ExtensionContainerScopeCache.TryCurrent == this)
 			{
-				ExtensionContainerScopeCache.current.Value = parent;
+				ExtensionContainerScopeCache.Current = parent;
 			}
 			base.Dispose();
 		}
